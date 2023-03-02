2dcd662d7982dfad22829c554774f1ded734d026
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-03-02 11:44:49 +0200

    pre release version

--- src/components/pokemon/pokemonInfo/PokemonInfo.jsx
+++ src/components/pokemon/pokemonInfo/PokemonInfo.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './PokemonInfo.css';
 function PokemonInfo({ pokemon }) {
-  // const a = pokemon.stats.map((stat) => console.log(stat.stat.name));
-  // const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`;
-  // console.log(imageUrl); // Log the URL to the console
-  // ...
+  const typeColors = {
+    normal: '#A8A878',
+    fire: '#F08030',
+    water: '#6890F0',
+    grass: '#78C850',
+    electric: '#F8D030',
+    ice: '#98D8D8',
+    fighting: '#C03028',
+    poison: '#A040A0',
+    ground: '#E0C068',
+    flying: '#A890F0',
+    psychic: '#F85888',
+    bug: '#A8B820',
+    rock: '#B8A038',
+    ghost: '#705898',
+    dragon: '#7038F8',
+    dark: '#705848',
+    steel: '#B8B8D0',
+    fairy: '#EE99AC',
+  };
 
   return (
     <div className='pokemon-info'>
@@ -17,7 +33,13 @@ function PokemonInfo({ pokemon }) {
       <div className='pokemon-name'>{pokemon.name}</div>
       <div className='pokemon-types'>
         {pokemon.types.map((type) => (
-          <div key={type.slot}>{type.type.name}</div>
+          <div
+            key={type.slot}
+            className='pokemon-type'
+            style={{ backgroundColor: typeColors[type.type.name] }}
+          >
+            {type.type.name}
+          </div>
         ))}
       </div>
       <div className='pokemon-stats'>
